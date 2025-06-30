3cdd4c12ebeaa59e8e2f1c28d4b5c4cd9a1c3e47
Alexander Kormanovsky <kormanowskij@yandex.ru>
2025-06-30 19:41:55 +0300

    working on translation;

--- ZoomScrollView/ViewController.m
+++ ZoomScrollView/ViewController.m
@@ -20,6 +20,7 @@
 @implementation ViewController
 {
     NSArray<UIView *> *_views;
+    CGFloat _requiredDistance;
 }
 
 - (void)viewDidLoad
@@ -27,6 +28,19 @@
     [super viewDidLoad];
     
     _views = @[self.view1, self.view2];
+    
+    _requiredDistance = [self calcDistanceWithView:self.view1];
+}
+
+- (CGFloat)calcDistanceWithView:(UIView *)view
+{
+    CGRect viewRect = [view convertRect:view.frame toView:nil];
+    CGRect centerViewRect = [self.centerView convertRect:self.centerView.frame toView:nil];
+    CGFloat dist = (view == _views.firstObject)
+    ? centerViewRect.origin.x - (viewRect.origin.x + viewRect.size.width) // left view
+    : viewRect.origin.x - (centerViewRect.origin.x + centerViewRect.size.width); // right view
+
+    return fabs(dist);
 }
 
 - (CGFloat)invertedZoomScale
@@ -36,18 +50,29 @@
 
 - (void)updateWithZoom:(CGFloat)zoom
 {
+    // using convertRect to get real size on screen
+    CGFloat centerViewWidth = [self.centerView convertRect:self.centerView.frame toView:nil].size.width;
+    
     for (UIView *view in _views) {
-        CGFloat tx = 0; // TODO: calculate
+        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(self.invertedZoomScale, self.invertedZoomScale);
+        view.transform = scaleTransform;
+    
+        CGFloat viewWidth = view.bounds.size.width;
+        // delta = (1 - S) * (P0 + (wB - S * wA) / 2)
+        
+        CGFloat tx = (1 - zoom) * (_requiredDistance + (50 - zoom * 50) / 2); // TODO: calculate
         CGFloat ty = 0;
         
         if ([view isEqual:_views.firstObject]) {
             tx *= -1;
         }
         
-        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(self.invertedZoomScale, self.invertedZoomScale);
         CGAffineTransform translationTransform = CGAffineTransformMakeTranslation(tx, ty);
+        view.transform = CGAffineTransformConcat(view.transform, translationTransform);
         
-        view.transform = CGAffineTransformConcat(scaleTransform, translationTransform);
+        if ([view isEqual:_views.firstObject]) {
+            NSLog(@"center view width %f, view width %f, tx %f", centerViewWidth, viewWidth, tx);
+        }
     }
 }
 
