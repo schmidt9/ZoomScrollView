9df3731c13b678b01867cdaf913eabda29103fc8
Alexander Kormanovsky <kormanowskij@yandex.ru>
2025-07-04 09:57:44 +0300

    rewrite translation;

--- ZoomScrollView/ViewController.m
+++ ZoomScrollView/ViewController.m
@@ -22,7 +22,6 @@
 {
     NSArray<UIView *> *_views;
     CGFloat _initialDistance;
-    CGFloat _initialViewsWidth;
 }
 
 - (void)viewDidLoad
@@ -38,8 +37,8 @@
 {
     [super viewDidAppear:animated];
     
-    _initialDistance = [self calcDistanceWithView:self.view2];
-    _initialViewsWidth = [self.centerView convertRect:self.view2.frame toView:nil].size.width;
+    // distance from center of "left-side-view" to leading edge of "center-view"
+    _initialDistance = CGRectGetMinX(_centerView.frame) - CGRectGetMidX(_view1.frame);
 }
 
 - (void)updateCurrentZoomLabel
@@ -52,51 +51,28 @@
     return 1.0 / self.scrollView.zoomScale;
 }
 
+/**
+ https://stackoverflow.com/a/79688031/3004003
+ */
 - (void)updateWithZoom:(CGFloat)zoom
 {
-    // using convert... methods to get real position and size of view scaled by the scroll view on the screen
-    CGFloat centerViewLeft = [self.centerView convertPoint:CGPointZero toView:nil].x;
-    CGFloat centerViewWidth = [self.centerView convertRect:self.centerView.frame toView:nil].size.width;
-    CGFloat centerViewRight = centerViewLeft + centerViewWidth;
-    
     for (UIView *view in _views) {
-        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(self.invertedZoomScale, self.invertedZoomScale);
-        view.transform = scaleTransform;
-        
-        CGRect viewBounds = [view convertRect:view.bounds toView:nil];
-        
-        // using initial view size because applying inverted transform keeps visual size of the transformed view,
-        // but convertRect method gives transformed size
-        CGFloat viewLeft = CGRectGetMidX(viewBounds) - _initialViewsWidth / 2;
-        CGFloat viewRight = CGRectGetMidX(viewBounds) + _initialViewsWidth / 2;
-        
-        BOOL isLeftView = (view == _views.firstObject);
-        CGFloat distance = isLeftView
-        ? centerViewLeft - viewRight
-        : viewLeft - centerViewRight;
-        CGFloat tx = (_initialDistance - distance) / zoom;
+        CGFloat tx = (_initialDistance * zoom) - _initialDistance;
         CGFloat ty = 0;
         
-        if (isLeftView) {
+        if (CGRectGetMidX(view.frame) > CGRectGetMidX(_centerView.frame)) {
+            // right-side-view, so negative translation
             tx *= -1;
         }
         
+        CGAffineTransform scaleTransform = CGAffineTransformMakeScale(self.invertedZoomScale, self.invertedZoomScale);
         CGAffineTransform translationTransform = CGAffineTransformMakeTranslation(tx, ty);
-        view.transform = CGAffineTransformConcat(view.transform, translationTransform);
+        
+        // translate, then scale
+        view.transform = CGAffineTransformConcat(translationTransform, scaleTransform);
     }
     
     [self updateCurrentZoomLabel];
-}
-
-- (CGFloat)calcDistanceWithView:(UIView *)view
-{
-    CGRect viewRect = [view convertRect:view.bounds toView:nil];
-    CGRect centerViewRect = [self.centerView convertRect:self.centerView.bounds toView:nil];
-    CGFloat dist = (view == _views.firstObject)
-    ? centerViewRect.origin.x - (viewRect.origin.x + viewRect.size.width) // left view
-    : viewRect.origin.x - (centerViewRect.origin.x + centerViewRect.size.width); // right view
-    
-    return fabs(dist);
 }
 
 #pragma mark - UI Events
